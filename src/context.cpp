313231d316f6eee284fb442ad3b007e37218c1bd
qkrwn1336 <coii0987@gmail.com>
2021-03-27 22:15:55 +0900

    week_04

--- src/context.cpp
+++ src/context.cpp
@@ -9,11 +9,12 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
   	
+	
   float vertices[] = {
-     0.5f,  0.5f, 0.0f, // top right
-     0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f,  0.5f, 0.0f, // top left
+    0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // top right, green
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right, red
+    -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // bottom left, yellow
+    -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top left, blue
   };
   uint32_t indices[] = { // note that we start from 0!
     0, 1, 3, // first triangle
@@ -22,16 +23,18 @@ bool Context::Init() {
 	
   	
   m_vertexLayout = VertexLayout::Create();
-  
-  m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER,GL_STATIC_DRAW, vertices, sizeof(float) * 12);
+
+  m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER,GL_STATIC_DRAW, vertices, sizeof(float) * 24);
 
 	
-  m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+  // m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+  m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+  m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
   m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
-  ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-  ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+  ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+  ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
   if (!vertShader || !fragShader)
     return false;
   SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -41,15 +44,18 @@ bool Context::Init() {
   if (!m_program)
     return false;
   SPDLOG_INFO("program id: {}", m_program->Get());
-
+  
   glClearColor(0.9f, 0.2f, 0.8f, 0.4f);
 
   return true;
 }
 	
 void Context::Render() {
-    glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT);
  
-    m_program->Use();
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  m_program->Use();
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+  //  m_program->Use();
+  //  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
