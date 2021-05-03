517ef872a59c91d2f41917404a547029c7edc90c
qkrwn1336 <coii0987@gmail.com>
2021-05-03 21:27:11 +0900

    LIGHTING MAPS

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,8 @@ void OnScroll(GLFWwindow* window, double xoffset, double yoffset) {
 }
 
 int main(int argc, const char** argv) {
-    // 시작을 알리는 로그
     SPDLOG_INFO("Start program");
 
-    // glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Initialize glfw");
     if (!glfwInit()) {
         const char* description = nullptr;
@@ -68,7 +66,6 @@ int main(int argc, const char** argv) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
     auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
       nullptr, nullptr);
@@ -79,7 +76,6 @@ int main(int argc, const char** argv) {
     }
     glfwMakeContextCurrent(window);
 
-    // glad를 활용한 OpenGL 함수 로딩
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     SPDLOG_ERROR("failed to initialize glad");
     glfwTerminate();
@@ -121,7 +117,6 @@ int main(int argc, const char** argv) {
     glfwSetMouseButtonCallback(window, OnMouseButton);
     glfwSetScrollCallback(window, OnScroll);
 
-    // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
